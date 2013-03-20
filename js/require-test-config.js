8e024c2a2eeef6f1163e83affbac48be463488a4
Brian Beckett <brian@worthlesscheese.com>
2013-03-20 03:26:10 +0000

    Add Sinon to require-test-config

--- js/require-test-config.js
+++ js/require-test-config.js
@@ -3,7 +3,8 @@ require.config({
 	paths: {
 		//Test
 		'jasmine': 'lib/jasmine-1.3.1/jasmine',
-		'jasmine-html': 'lib/jasmine-1.3.1/jasmine-html'
+		'jasmine-html': 'lib/jasmine-1.3.1/jasmine-html',
+		'sinon': 'lib/sinon/sinon-1.6.0'
 	},
 
 	shim: {
@@ -12,6 +13,9 @@ require.config({
 		},
 		'jasmine': {
 			exports: 'jasmine'
+		},
+		'sinon': {
+			exports: 'sinon'
 		}
 	}
 });
