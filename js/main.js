dd4ced5a14e1738d873b44849bcfdb1a9b33059a
Brian Beckett <brian@worthlesscheese.com>
2013-04-01 17:51:18 +0100

    Hang onto app for later

--- js/main.js
+++ js/main.js
@@ -10,11 +10,11 @@
 		, 'jquery'
 	], function (config, App, Router, Backbone, $) {
 
-		var app = new App({
+		this.app = new App({
 			el: $('.page')
 		})
-		app.render()
-		this.router = new Router(app.pages)
+		this.app.render()
+		this.router = new Router(this.app.pages)
 		Backbone.history.start()
 
 		//If we're not on a page yet, default to the Splashscreen page
